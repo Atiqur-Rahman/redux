9d788db3e1956e81193e9920ba0dedc6798f3883
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-10-25 22:02:33 +0600

    redux basic scoreboard project

---
+++ script.js
@@ -0,0 +1,159 @@
+let matchesCounter = document.querySelector('.all-matches');
+let addMatch = document.querySelector('.lws-addMatch');
+let resetMatch = document.querySelector('.lws-reset');
+
+const INCREMENT = 'INCREMENT';
+const DECREMENT = 'DECREMENT';
+const ADD_MATCH = 'ADD_MATCH';
+const RESET_MATCH = 'RESET_MATCH';
+const DELETE = 'DELETE';
+
+// initial state
+let initialState = [
+    {
+        match: 'MATCH 1',
+        score: 120,
+        id: 1,
+    },
+];
+
+// create reducer function
+function MatchesReducer(state = initialState, action) {
+    switch (action.type) {
+        case INCREMENT: {
+            let updatedScore = state.map((match) => {
+                if (match.id === +action.payload.id) {
+                    return {
+                        ...match,
+                        score: match.score + action.payload.score,
+                    };
+                } else {
+                    return match;
+                }
+            });
+            return updatedScore;
+        }
+        case DECREMENT: {
+            let updatedScore = state.map((match) => {
+                if (match.id === +action.payload.id) {
+                    return {
+                        ...match,
+                        score: Math.max(0, match.score - action.payload.score),
+                    };
+                } else {
+                    return match;
+                }
+            });
+            return updatedScore;
+        }
+        case ADD_MATCH: {
+            let new_match = {
+                match: 'MATCH ' + (state.length + 1),
+                score: 0,
+                id: state.length + 1,
+            };
+            return [...state, new_match];
+        }
+        case RESET_MATCH: {
+            for (let i = 0; i < state.length; i++) {
+                state[i].score = 0;
+            }
+            return state;
+        }
+        case DELETE: {
+            return state.filter((obj) => obj.id !== +action.payload.id);
+        }
+        default: {
+            return state;
+        }
+    }
+}
+
+const store = Redux.createStore(MatchesReducer);
+
+const render = () => {
+    matchesCounter.innerHTML = '';
+    const state = store.getState();
+    state.map((data) => {
+        matchesCounter.innerHTML += `
+        <div class="match">
+            <div class="wrapper">
+                <button class="lws-delete" onClick="handleDelete.call(this, event)">
+                    <img data-id=${data.id} src="./image/delete.svg" alt="" />
+                </button>
+                <h3 class="lws-matchName">${data.match}</h3>
+            </div>
+            <div class="inc-dec">
+                <form class="incrementForm" data-id=${data.id} onSubmit="handleIncrement.call(this, event)">
+                    <h4>Increment</h4>
+                    <input type="number" name="increment" class="lws-increment" />
+                </form>
+                <form class="decrementForm" data-id=${data.id} onSubmit="handleDecrement.call(this, event)">
+                    <h4>Decrement</h4>
+                    <input type="number" name="decrement" class="lws-decrement" />
+                </form>
+            </div>
+            <div class="numbers">
+                <h2 class="lws-singleResult">${data.score}</h2>
+            </div>
+        </div>
+        `;
+    });
+};
+
+render();
+store.subscribe(render);
+
+function handleIncrement(event) {
+    event.preventDefault();
+    const incrementInput = this.elements['increment'];
+    const incrementValue = incrementInput.value;
+    const matchID = event.target.dataset.id;
+
+    store.dispatch({
+        type: INCREMENT,
+        payload: {
+            score: +incrementValue,
+            id: matchID,
+        },
+    });
+}
+
+function handleDecrement(event) {
+    event.preventDefault();
+    const decrementInput = this.elements['decrement'];
+    const decrementValue = decrementInput.value;
+    const matchID = event.target.dataset.id;
+
+    store.dispatch({
+        type: DECREMENT,
+        payload: {
+            score: +decrementValue,
+            id: matchID,
+        },
+    });
+}
+
+function handleDelete(event) {
+    event.preventDefault();
+    const matchID = event.target.dataset.id;
+
+    store.dispatch({
+        type: DELETE,
+        payload: {
+            id: matchID,
+        },
+    });
+}
+
+addMatch.addEventListener('click', () => {
+    store.dispatch({
+        type: ADD_MATCH,
+    });
+});
+
+resetMatch.addEventListener('click', () => {
+    store.dispatch({
+        type: RESET_MATCH,
+    });
+});
