94ff19aaf669627cc399304a2019ce9a3824b541
Atiqur-Rahman <rahmanatique88@gmail.com>
2023-10-25 23:44:46 +0600

    component is updated

--- src/App.js
+++ src/App.js
@@ -1,68 +1,17 @@
-import { useState } from 'react';
-import './App.css';
-import Counter from './components/Counter';
-import Stat from './components/Stat';
+import Counter from "./components/Counter";
 
-const initialValue = [
-    {
-        id: 1,
-        count: 0,
-    },
-    {
-        id: 2,
-        count: 0,
-    },
-    {
-        id: 3,
-        count: 0,
-    },
-];
-
-function App() {
-    const [state, setState] = useState(initialValue);
-
-    const totalCount = () => {
-        return state.reduce((total, counter) => total + counter.count, 0);
-    };
-
-    const increment = (id) => {
-        const updatedCount = state.map((c) => {
-            if (c.id === id) {
-                return {
-                    ...c,
-                    count: c.count + 1,
-                };
-            }
-            return { ...c };
-        });
-        setState(updatedCount);
-    };
-
-    const decrement = (id) => {
-        const updatedCount = state.map((c) => {
-            if (c.id === id) {
-                return {
-                    ...c,
-                    count: c.count - 1,
-                };
-            }
-            return { ...c };
-        });
-        setState(updatedCount);
-    };
-
+export default function App() {
     return (
         <div className="w-screen h-screen p-10 bg-gray-100 text-slate-700">
-            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">Simple Counter Application</h1>
-            <div className="max-w-md mx-auto mt-10 space-y-5">
-                {state.map((count) => (
-                    <Counter key={count.id} id={count.id} count={count.count} increment={increment} decrement={decrement} />
-                ))}
+            <h1 className="max-w-md mx-auto text-center text-2xl font-bold">
+                Simple Counter Application
+            </h1>
 
-                <Stat count={totalCount()} />
+            <div className="max-w-md mx-auto mt-10 space-y-5">
+                <Counter />
+                <Counter />
+                <Counter />
             </div>
         </div>
     );
 }
-
-export default App;
